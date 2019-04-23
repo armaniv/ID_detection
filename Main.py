c57e7cc2bde016f68c1756dfba3432e815b4655a
armaniv <valentino.armani@studenti.unitn.it>
2019-04-23 16:29:55 +0200

    Minor fix and fist quantile

--- Main.py
+++ Main.py
@@ -4,25 +4,26 @@ import os
 import json
 import cv2
 import numpy as np
-# from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
+
 
 PATH = '/media/vale/Elements/Vale/Dataset Images'  # the path where the dataset is located
 
 
-def compute_image_histogram(img_name, quadrant, use_mask=True):
+def compute_image_histogram(img_name, quadrangle, use_mask=True):
     """
         Method that returns the histogram of the passed image. If 'use_mask' is set to True the method compute the
-        histogram of the 'quadrant' only
+        histogram of the 'quadrangle' only
 
         Parameters
         ----------
         img_name : str
             The path to the image that contains the identity document
-        quadrant: list
+        quadrangle: list
             The list of coordinates [x,y] (top-left, top-right, bottom-right, bottom-left) of the identity document
             within the passed image
         use_mask: bool
-            Specify if apply a mask to the image in order to consider the quadrant only. Default = True
+            Specify if apply a mask to the image in order to consider the quadrangle only. Default = True
 
 
         Returns
@@ -36,34 +37,29 @@ def compute_image_histogram(img_name, quadrant, use_mask=True):
 
     if use_mask:
         # get the coordinate of the 4 vertices of the identity document (tl -> top left, br -> bottom right, ...)
-        x_tl = quadrant[0][0]
-        y_tl = quadrant[0][1]
-        x_tr = quadrant[1][0]
-        y_tr = quadrant[1][1]
-        x_br = quadrant[2][0]
-        y_br = quadrant[2][1]
-        x_bl = quadrant[3][0]
-        y_bl = quadrant[3][1]
+        x_tl = quadrangle[0][0]
+        y_tl = quadrangle[0][1]
+        x_tr = quadrangle[1][0]
+        y_tr = quadrangle[1][1]
+        x_br = quadrangle[2][0]
+        y_br = quadrangle[2][1]
+        x_bl = quadrangle[3][0]
+        y_bl = quadrangle[3][1]
 
-        # get the max height and width (necessary for any rotated documents)
-        w = max(abs(x_tl - x_br), abs(x_bl - x_tr))
-        h = max(abs(y_tl - y_br), abs(y_bl - y_tr))
-
-        # get the coordinates where start to cut off (necessary for any rotated documents)
-        x_start = min(x_tl, x_bl)
-        y_start = min(y_tl, y_tr)
-
-        # create a mask with the computed values
+        # create and apply the mask (and visualize)
+        rect = [[x_br, y_br], [x_bl, y_bl], [x_tl, y_tl], [x_tr, y_tr]]
+        poly = np.array([rect], dtype=np.int32)
         mask = np.zeros(img.shape[:2], np.uint8)
-        mask[y_start:y_start + h, x_start:x_start + w] = 255
+        cv2.fillPoly(mask, poly, 255)
+        # masked_img = cv2.bitwise_and(img, img, mask=mask)
+        # plt.imshow(masked_img)
+        # plt.title(img_name[-12:])
+        # plt.show()
 
     if use_mask:
         histogram = cv2.calcHist([img], [0, 1], mask, [180, 256], [0, 180, 0, 256])
     else:
         histogram = cv2.calcHist([img], [0, 1], None, [180, 256], [0, 180, 0, 256])
-
-    # plt.plot(histogram)
-    # plt.show()
 
     return histogram
 
@@ -86,7 +82,7 @@ def get_position(dir_name, subdir_name, img_name):
     -------
     fully : bool
         True if the image fully contains a identity document
-    quadrant : list
+    quadrangle : list
         The list of coordinates [x,y] (top-left, top-right, bottom-right, bottom-left) of the identity document
     """
 
@@ -94,12 +90,12 @@ def get_position(dir_name, subdir_name, img_name):
     file_name = path_json + img_name[:-4] + '.json'
 
     fully_inside = False
-    quadrant = []  # the quadrant that contains the identity document
+    quadrangle = []  # the quadrant that contains the identity document
     i = 0  # counter of vertices satisfying the constraints
     with open(file_name) as f:
         data = json.load(f)
-        quadrant = data['quad'].copy()
-        for xy in quadrant:
+        quadrangle = data['quad'].copy()
+        for xy in quadrangle:
             if (xy[0] >= 0) and (xy[0] <= 1080):
                 if xy[1] >= 0 and (xy[1] <= 1920):
                     i += 1
@@ -107,51 +103,83 @@ def get_position(dir_name, subdir_name, img_name):
     if i == 4:  # all 4 vertices are within the image
         fully_inside = True
 
-    return fully_inside, quadrant
+    return fully_inside, quadrangle
 
 
-def find_best_image():
+def compute_distances():
     """
-    Wrapper method that find for each folder inside the MIDV-500 dataset the best image (the one with the least
-    Kullback-Leibler divergence wrt the base image of the document)
+    Method that for each images (that fully contains a document) inside the MIDV-500 dataset compute the
+    Kullback-Leibler divergence wrt the base image of the document.
 
     Returns
     -------
-    best_images : dict
-        The dictionary that contains the selected images
+    kl_distances : dict
+        The dictionary that contains all the KL divergences computed
     """
 
     document_directories = next(os.walk(PATH))[1]  # get the (50) 'global' directories
-    best_images = dict.fromkeys(document_directories)  # dict. with the best image for each folder of each document
+    kl_distances = dict.fromkeys(document_directories)  # global dictionary
 
     for document in document_directories:
         path_img_dir = PATH + '/' + document + '/images'  # the 'images' directory of the current document
         img_directories = next(os.walk(path_img_dir))[1]  # get the (10) image directories of the document
-        best_img_sub_dict = dict.fromkeys(img_directories)  # dict. with the best image for each folder
+        distances_img_sub_dict = dict.fromkeys(img_directories)  # sub-dictionary
         base_histogram = compute_image_histogram(path_img_dir + '/' + document + '.tif', [],
-                                                 False)  # compute the histogram of the base image, it will be used as
-                                                         # reference for all the comparisisons
-
+                                                 False)  # compute the histogram of the base image
         for img_dir in img_directories:
             path_img = path_img_dir + '/' + img_dir
             images = os.listdir(path_img)  # get the (30) images inside the directory
-
+            distance_values = []  # list of kl distances (tuples of(image name, KL distance))
             for img in images:
-                fully_inside, quadrant = get_position(document, img_dir, img)
-                if fully_inside:  # could be that a directory doesn't contain any image with a document fully within
-                    hstgr = compute_image_histogram(path_img + '/' + img, quadrant)
-                    comp_value = cv2.compareHist(base_histogram, hstgr, cv2.HISTCMP_KL_DIV)
-                    if best_img_sub_dict[img_dir] is None:  # if there isn't already one image set as the best
-                        best_img_sub_dict[img_dir] = [img, comp_value]
-                    else:
-                        best_sofar_value = best_img_sub_dict[img_dir][1]
-                        if comp_value < best_sofar_value:
-                            best_img_sub_dict[img_dir] = [img, comp_value]
+                fully_inside, quadrangle = get_position(document, img_dir, img)
+                if fully_inside:  # check if the image fully contains a document
+                    hstgr = compute_image_histogram(path_img + '/' + img, quadrangle)  # compute histogram
+                    comp_value = cv2.compareHist(base_histogram, hstgr, cv2.HISTCMP_KL_DIV)  # compute difference
+                    distance_values.append((img, comp_value))  # append to the list
+            distances_img_sub_dict[img_dir] = distance_values
 
-        best_images[document] = best_img_sub_dict
+        kl_distances[document] = distances_img_sub_dict
+
+    return kl_distances
+
+
+def find_best_images(kl_dist):
+    """
+        Method that return the images that have a Kullback-Leibler divergence lower than a fixed threshold
+        (the first quantile)
+
+        Parameters
+        ----------
+        kl_dist : dict
+            The dictionary containing the KL divergences stored as tuples of(image name, KL distance)
+
+        Returns
+        -------
+        best_images : dict
+            The dictionary that contains the selected images
+        """
+    best_images = dict.fromkeys(kl_dist.keys())  # new global dictionary (same structure as the passed one)
+
+    for k1, v1 in kl_dist.items():  # iterate through the passed dictionary
+        best_img_sub_dict = {}  # new sub-dictionary (same structure as the passed one)
+        for k2, v2 in v1.items():   # iterate through the sub-dictionary (of the passed dict.)
+            best_img_sub_dict[k2] = []
+
+            if len(v2) > 0:     # if the list is not empty
+                distances = []  # list of all distances
+                for i in v2:
+                    distances.append(i[1])      # get (and append) the value of kl distance
+                threshold = np.quantile(distances, 0.25)    # compute the first quantile of the list of distances
+                for j in v2:
+                    if j[1] <= threshold:       # if the distance is lower than the threshold
+                        best_img_sub_dict[k2].append(j)     # add to the new sub-dictionary the current tuple
+
+        best_images[k1] = best_img_sub_dict
 
     return best_images
 
 
-dict_best = find_best_image()
-print(json.dumps(dict_best, indent=2))
+kl_dict = compute_distances()
+best_dict = find_best_images(kl_dict)
+
+print(json.dumps(best_dict, indent=2))
