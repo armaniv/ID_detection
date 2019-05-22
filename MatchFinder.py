ca761becbf31a2ab15ae5fc3a73189ca215437c9
armaniv <valentino.armani@studenti.unitn.it>
2019-05-22 13:17:32 +0200

    Refactor2

--- MatchFinder.py
+++ MatchFinder.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
+# code taken from https://docs.opencv.org/3.4.1/d1/de0/tutorial_py_feature_homography.html
 
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-# code take from https://docs.opencv.org/3.4.1/d1/de0/tutorial_py_feature_homography.html
-
 PATH = '/media/vale/HDDVale/Uni/DataSet'
 MIN_MATCH_COUNT = 10
 FLANN_INDEX_KDTREE = 1
 
-img1 = cv.imread('/home/vale/Scaricati/model_30_ita_drvlic.tif', 1)     # queryImage
-img2 = cv.imread('/home/vale/Scaricati/ita_drvid_2.jpg', 1)             # trainImage
+img1 = cv.imread(PATH + '/30_ita_drvlic/images/30_ita_drvlic.tif', 1)     # queryImage
+img2 = cv.imread(PATH + '/30_ita_drvlic/images/CA/CA30_02.tif', 1)             # trainImage
 
 sift = cv.xfeatures2d.SIFT_create()                                     # Initiate SIFT detector
 
@@ -26,17 +25,17 @@ flann = cv.FlannBasedMatcher(index_params, search_params)
 matches = flann.knnMatch(des1, des2, k=2)
 
 # store only good matches (Lowe's ratio test)
-good = []
+goods = []
 for m, n in matches:
     if m.distance < 0.74 * n.distance:
-        good.append(m)
+        goods.append(m)
 
-print("Found %s match/es" % (len(good)))
+print("Found %s matches" % (len(goods)))
 
-if len(good) > MIN_MATCH_COUNT:                                         # the minimum accepted number of matches is 4
+if len(goods) > MIN_MATCH_COUNT:                                         # the minimum accepted number of matches is 4
     # extract the locations of matched keypoints
-    src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+    src_pts = np.float32([kp1[m.queryIdx].pt for m in goods]).reshape(-1, 1, 2)
+    dst_pts = np.float32([kp2[m.trainIdx].pt for m in goods]).reshape(-1, 1, 2)
 
     # find the perspective transformation
     M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
@@ -55,7 +54,7 @@ else:
 
 # draw a properly formatted image with the matches found
 draw_params = dict(matchColor=(0, 255, 0), singlePointColor=None, matchesMask=matchesMask, flags=2)
-img3 = cv.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
+img3 = cv.drawMatches(img1, kp1, img2, kp2, goods, None, **draw_params)
 
 plt.imshow(img3)
 plt.axis('off')
